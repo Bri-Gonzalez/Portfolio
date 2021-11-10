d918521b02342a914e590a51fad3d8954c620227
Briana Gonzalez <brianagonzalez@Brianas-MacBook-Air.local>
2021-11-10 17:35:32 -0600

    setup App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout'
+import MainContainer from './container/MainContainer'
 
 
 function App() {
   return (
     <div className="App">
-
+      <Layout>
+        <Routes>
+          <Route path='/' element={<MainContainer/>} />
+        </Routes>
+      </Layout>
     </div>
-  );
+  )
 }
 
 export default App
