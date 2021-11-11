c2b4e23cdff6b03f0295cba074829b1952d864b9
Briana Gonzalez <brianagonzalez@Brianas-MacBook-Air.local>
2021-11-11 14:12:20 -0600

    project 3 complete with CSS

--- client/src/components/Projects/Project3/Project3.jsx
+++ client/src/components/Projects/Project3/Project3.jsx
@@ -1,10 +1,49 @@
-import './Project3.css'
+import LaunchIcon from '@mui/icons-material/Launch'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 function Project3() {
   return (
-    <div>
-      Project 3
-    </div>
+    <section>
+      <div className='project-left'>
+        <div className='left-img'>
+          <img
+            src='https://res.cloudinary.com/dfryxohde/image/upload/v1636575404/Portfolio/Screen_Shot_2021-11-09_at_11.22.48_PM_m6slmo.png'
+            alt='PokeBinder'
+          />
+        </div>
+        <div className='left-details'>
+          <p className='left-title'>PokéBinder</p>
+          <div className='left-description-container'>
+            <div className='left-description'>
+              <p>
+                Front end full CRUD application utilizing Airtable that was
+                created as an extension to PokéApps. PokéBinder allows the user
+                to upload their favorite Pokémon cards to a virtual binder.
+              </p>
+            </div>
+          </div>
+          <div className='left-tech'>
+            <p>React, CSS</p>
+          </div>
+        </div>
+      </div>
+      <div className='left-links'>
+        <a
+          href='https://pokebinder.netlify.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <LaunchIcon />
+        </a>
+        <a
+          href='https://github.com/Bri-Gonzalez/PokeBinder'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GitHubIcon />
+        </a>
+      </div>
+    </section>
   )
 }
 
