336b2f69c364134c4b0d5fef6b19c0ca2b9e94fd
Briana Gonzalez <brianagonzalez@Brianas-MacBook-Air.local>
2021-11-10 20:28:26 -0600

    saving Home CSS progress

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -3,7 +3,22 @@ import './Home.css'
 function Home() {
   return (
     <div className='home-container'>
-      Home
+      <h1>briana gonzalez.</h1>
+      <div className='intro-container'>
+        <div className='profile-img'>
+          <img
+            src='https://res.cloudinary.com/dfryxohde/image/upload/v1636594910/Portfolio/github_pic_iqgxua.jpg'
+            alt='briana gonzalez'
+          />
+        </div>
+        <div className='call-to-action'>
+          <p>
+            I’m a full stack developer that enjoys creating visually appealing
+            and user-friendly applications.
+          </p>
+          <button>check out my work!</button>
+        </div>
+      </div>
     </div>
   )
 }
