72f4a108609336887009c21b5248a4097252c9df
Briana Gonzalez <brianarachelgonzalez@gmail.com>
2021-12-26 14:37:13 -0600

    fixed typo in about me

--- client/src/screens/AboutMe/AboutMe.jsx
+++ client/src/screens/AboutMe/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
     <section id='about' className='about'>
       <h2>about me.</h2>
       <div className='about-details'>
-        <p>Hello! My name is Briana Gonzalez, and I’m a software engineer with a background in education and online streaming. In my previous roles, I have had the opportunity to work in teams with others as well manage multiple communities. I’m passionate about learning and creating applications on the internet and am looking forward to bringing my optimistic approach to my next role. </p>
+        <p>Hello! My name is Briana Gonzalez, and I’m a software engineer with a background in education and online streaming. In my previous roles, I have had the opportunity to work in teams with others as well as manage multiple communities. I’m passionate about learning and creating applications on the internet and am looking forward to bringing my optimistic approach to my next role. </p>
         <p>Here is a list of technologies I have worked with: </p>
       </div>
       <div className='about-tech-container'>
